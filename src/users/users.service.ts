36fbaad9d4ef5a60447db50c99639982f936824b
nathandjianmartin <ndmroot-git@yahoo.fr>
2021-12-21 10:21:07 +0100

    Created passport local strategy file.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
 
+// This should be a real class/interface representing a user entity
+export type User = any;
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    private readonly users = [
+        {
+            userId: 1,
+            username: 'nathan',
+            password: 'soundboks',
+        },
+        {
+            userId: 2,
+            username: 'margot',
+            password: 'ruby',
+        },
+    ];
+
+    async findOne(username: string): Promise<User | undefined> {
+        return this.users.find(user => user.username === username);
+    }
+}
