6d658aa30b837bb2533e6fd6b811c9b6e54f70e5
nathandjianmartin <ndmroot-git@yahoo.fr>
2021-12-21 10:54:07 +0100

    The auth/login route now returns a JWT.

---
+++ src/auth/constants.ts
@@ -0,0 +1,4 @@
+export const jwtConstants = {
+    secret: 'secret-dev-key'
+    // TODO read secret from .env file (see https://stackoverflow.com/questions/55673424/nestjs-unable-to-read-env-variables-in-module-files-but-able-in-service-files)
+}
