bb7f351eb2a1540ac0abb17a8049d27bd6fd2223
Margot <margot-georget@hotmail.fr>
2021-12-30 17:54:01 +0100

    Fixed bug

--- src/cost-data/cost-data.module.ts
+++ src/cost-data/cost-data.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CostDataService } from './cost-data.service';
 import { CostDataController } from './cost-data.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CostData } from './entities/cost-data.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CostData])],
   controllers: [CostDataController],
   providers: [CostDataService]
 })
