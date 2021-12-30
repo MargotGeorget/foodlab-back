388638e5f0acd316d7a3835015e110e78ccad45d
Margot <margot-georget@hotmail.fr>
2021-12-30 17:28:52 +0100

    Added function in cost data service

--- src/cost-data/cost-data.service.ts
+++ src/cost-data/cost-data.service.ts
@@ -1,26 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCostDataDto } from './dto/create-cost-data.dto';
 import { UpdateCostDataDto } from './dto/update-cost-data.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CostData } from './entities/cost-data.entity';
 
 @Injectable()
 export class CostDataService {
-  create(createCostDatumDto: CreateCostDataDto) {
-    return 'This action adds a new costDatum';
+
+  constructor(
+    @InjectRepository(CostData)
+    private costDataRepository: Repository<CostData>,
+  ) {}
+
+  create(createCostDataDto: CreateCostDataDto) {
+    //'This action adds a new costData'
+    return this.costDataRepository.save(createCostDataDto);
   }
 
   findAll() {
-    return `This action returns all costData`;
+    //`This action returns all costData`
+    return this.costDataRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} costDatum`;
+    //`This action returns a #${id} costData`
+    return this.costDataRepository.findOne({id: id});
   }
 
-  update(id: number, updateCostDatumDto: UpdateCostDataDto) {
-    return `This action updates a #${id} costDatum`;
+  update(id: number, updateCostDataDto: UpdateCostDataDto) {
+    //`This action updates a #${id} costData`
+    return this.costDataRepository.update({id: id}, updateCostDataDto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} costDatum`;
+    //`This action removes a #${id} costData`
+    //TODO: vérifier que c'est pas le cout pas défaut
+    return this.costDataRepository.delete({id: id});
   }
 }
