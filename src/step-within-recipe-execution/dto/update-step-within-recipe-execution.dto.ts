da2a9799ff4cee977a17ce1267ebca834b04082f
Margot <margot-georget@hotmail.fr>
2022-01-07 16:05:46 +0100

    Added verification in number in step in recipe execution

--- src/step-within-recipe-execution/dto/update-step-within-recipe-execution.dto.ts
+++ src/step-within-recipe-execution/dto/update-step-within-recipe-execution.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateStepWithinRecipeExecutionDto } from './create-step-within-recipe-execution.dto';
 
-export class UpdateStepWithinRecipeExecutionDto extends PartialType(CreateStepWithinRecipeExecutionDto) {}
+export class UpdateStepWithinRecipeExecutionDto extends PartialType(CreateStepWithinRecipeExecutionDto) {
+  id: number
+}
--- src/step-within-recipe-execution/step-within-recipe-execution.controller.ts
+++ src/step-within-recipe-execution/step-within-recipe-execution.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpException, HttpStatus } from '@nestjs/common';
 import { StepWithinRecipeExecutionService } from './step-within-recipe-execution.service';
 import { CreateStepWithinRecipeExecutionDto } from './dto/create-step-within-recipe-execution.dto';
 import { UpdateStepWithinRecipeExecutionDto } from './dto/update-step-within-recipe-execution.dto';
@@ -32,6 +32,34 @@ export class StepWithinRecipeExecutionController {
   findAllProgressionInRecipeExecution(@Param('id') id: string) {
     return this.stepWithinRecipeExecutionService.findAllProgressionInRecipeExecution(+id)}
 
+  @Patch('update-all')
+  updateAllStepsWithinRecipeExecution(@Body() updateStepsWithinRecipeExecutionDto: UpdateStepWithinRecipeExecutionDto[]){
+    console.log(updateStepsWithinRecipeExecutionDto);
+    //Vérifier qu'aucune valeur number soit supérieur au nombre de valeur
+    let valid = updateStepsWithinRecipeExecutionDto.every(step =>
+      step.number <= updateStepsWithinRecipeExecutionDto.length);
+    //Vérifier que toutes les valeurs sont différentes
+    if (valid) {
+      valid = updateStepsWithinRecipeExecutionDto.every(data1 =>
+        updateStepsWithinRecipeExecutionDto.every((data2) =>
+          data1 === data2 || data1.number != data2.number));
+    }
+    let res = [];
+    if (valid){
+      console.log("dans la if");
+      for(let updateStepWithinRecipeExecutionDto of updateStepsWithinRecipeExecutionDto){
+        res.push(this.stepWithinRecipeExecutionService.update(updateStepWithinRecipeExecutionDto.id, updateStepWithinRecipeExecutionDto))
+      }
+    } else {
+      console.log("dans le else");
+      throw new HttpException({
+        status : HttpStatus.CONFLICT,
+        error: 'Number of step must be different and between 1 and ' + updateStepsWithinRecipeExecutionDto.length,
+      }, HttpStatus.CONFLICT);
+    }
+    console.log(res);
+    return res;
+  }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateStepWithinRecipeExecutionDto: UpdateStepWithinRecipeExecutionDto) {
